e7a9fe407cff3c45d983f117dae7818ce62eb17e
Toan Nguyen <toan.nguyenminh@hcmut.edu.vn>
2023-10-17 20:47:02 +0700

    fix bug upload

--- frontend/src/pages/Print/Upload.jsx
+++ frontend/src/pages/Print/Upload.jsx
@@ -1,7 +1,6 @@
 import React, {useEffect} from 'react'
 import { Box } from '@mui/material'
 import Header from '../../component/Header'
-import Sidebar from '../../component/Sidebar'
 const PageExample = () => {
   useEffect(()=>{
     document.title = 'Print | BKPRINTER'
