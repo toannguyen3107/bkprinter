5142ccf27c13af7deeba9318fd1cbc11574a7c73
truongminhtrihc <truongminhtringuyenvantu@gmail.com>
2023-10-23 18:56:21 +0700

    update edit

--- frontend/src/pages/PrinterList/Table.jsx
+++ frontend/src/pages/PrinterList/Table.jsx
@@ -15,6 +15,7 @@ import PrintIcon from '@mui/icons-material/Print';
 import PrintDisabledIcon from '@mui/icons-material/PrintDisabled';
 import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
 import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
+import EditInfoPrinter from '../Edit_info_printer/edit_info';
 import { Link } from 'react-router-dom'
 
 const maxRows = 10;
@@ -60,6 +61,17 @@ function isPrintable (pages, state) {
         </Link>
     );
 }
+////////////////////////////  EDIT INFO PRINTER///////////////////////////////
+function isEdit (id) {
+   return (
+      <Link to={`/edit_info_printer`}>
+           {id}
+      </Link>
+      // <EditInfoPrinter> {id}</EditInfoPrinter>
+  );
+  }
+////////////////////////////  EDIT INFO PRINTER///////////////////////////////
+
 export const PrinterTable = ({searchstring, rows}) => {
     const [page, setPage] = React.useState(0);
     // const [rowsPerPage, setRowsPerPage] = React.useState(5);
@@ -103,10 +115,14 @@ export const PrinterTable = ({searchstring, rows}) => {
                     key={row.id}
                     sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                     >
-                    <TableCell component="th" scope="rows">
-                        {row.id}
+
+                      {/* ////////////////////////////  EDIT INFO PRINTER/////////////////////////////// */}
+                    <TableCell component="th" scope="rows" >
+                        {isEdit(row.id)}
                     </TableCell>
-                    <TableCell align="left">{row.name}</TableCell>
+                    {/* ////////////////////////////  EDIT INFO PRINTER/////////////////////////////// */}
+
+                    <TableCell align="left">{row.name}  </TableCell>
                     <TableCell align="left">{row.location}</TableCell>
                     <TableCell align="left">{row.state}</TableCell>
                     <TableCell align="left">{row.remainingPage}</TableCell>
