747aa2cc093696381cd2ed48b48b4c0fdcee1c06
Toan Nguyen <toan.nguyenminh@hcmut.edu.vn>
2023-12-21 15:46:07 +0700

    implement be for login

---
+++ server/controllers/loginController.js
@@ -0,0 +1,43 @@
+import jwt from 'jsonwebtoken';
+import User from '../models/UserModel.js';
+import { StatusCodes } from 'http-status-codes';
+
+const accessTokenSecret = 'secret';
+
+const login = async (req, res) => {
+    // Read email and password from the request body
+    const { email, password } = req.body;
+
+    try {
+        // Find user from the database by email and password
+        const user = await User.findOne({ email, password });
+
+        if (user) {
+            // Generate an access token
+            const accessToken = jwt.sign({ username: user.username, role: user.role }, accessTokenSecret);
+
+            res.json({
+                accessToken
+            });
+        } else {
+            res.status(StatusCodes.UNAUTHORIZED).send('Username or password incorrect');
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).send('Internal Server Error');
+    }
+};
+
+const checkRole = (req, res) => {
+    try {
+        // Extract the user's role from the decoded token
+        const { role } = req.user;
+
+        res.status(StatusCodes.OK).json({ role }); // Send the role as a JSON response
+    } catch (error) {
+        console.error(error);
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).send('Internal Server Error');
+    }
+};
+
+export { login, checkRole };
